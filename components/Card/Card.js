946116335a6a807be3fccedd1189361fa7ed120f
Anton Roters <antonroters@hotmail.com>
2023-03-05 19:05:51 +0100

    add answer options to card

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -13,7 +13,54 @@ export function Card(data) {
   heading.classList.add("card__heading");
   heading.textContent = question;
 
-  card.append(heading);
+  // Create answer form
+  const form = document.createElement("form");
+  form.classList.add("card__answer-form");
+
+  const fieldset = document.createElement("fieldset");
+
+  const legend = document.createElement("legend");
+  legend.textContent = "Take your pick!";
+
+  const answerContainer = document.createElement("section");
+  answerContainer.classList.add("card__answer-container");
+
+  data.answers.forEach((answer, index) => {
+    answerInput(answerContainer, answer, index);
+  });
+
+  const submit = document.createElement("button");
+  submit.classList.add("button");
+  submit.setAttribute("type", "submit");
+  submit.textContent = "Confirm";
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const dataObject = Object.fromEntries(formData);
+    console.log(dataObject);
+    if (dataObject.answer === data.object[data.property]) {
+      console.log(true);
+    } else console.log(false);
+  });
+  fieldset.append(legend, answerContainer, submit);
+  form.append(fieldset);
+  card.append(heading, form);
 
   return card;
+}
+
+function answerInput(container, text, id) {
+  const input = document.createElement("input");
+  const label = document.createElement("label");
+
+  input.setAttribute("type", "radio");
+  input.setAttribute("name", "answer");
+  input.setAttribute("id", `answer-${id}`);
+  input.setAttribute("value", `${text}`);
+
+  label.setAttribute("for", `answer-${id}`);
+  label.textContent = text;
+  container.append(input, label);
 }
--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const cardSection = document.querySelector(".card-container");
 const fetchButton = document.querySelector('[data-js="fetch-question"]');
 
 fetchButton.addEventListener("click", async () => {
-  fetchButton.disabled = true;
   const data = await buildData();
   console.log(data);
   const card = Card(data);
   cardSection.append(card);
-  fetchButton.disabled = false;
 });
